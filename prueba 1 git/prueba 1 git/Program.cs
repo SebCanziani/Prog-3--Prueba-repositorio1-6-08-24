59cb09f03c21441d7356f9a9b5143d668ea9625c
unknown <sebicanziani@gmail.com>
2024-08-06 20:19:05 -0300

    prueba 1 git

---
+++ prueba 1 git/prueba 1 git/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prueba_1_git
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+  
+
+            Console.WriteLine("Hola Mundo");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
